194b99d61ed8af8f7928d34da6ac07f036335f73
your_github_username <your_github_email>
2022-03-10 19:27:17 +0530

    Added:JUNIT TESTING

---
+++ src/main/java/com/bridgelabz/LambdaExpression/Day23/HappyOrSad.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.LambdaExpression.Day23;
+
+public class HappyOrSad {
+    public static String moodanalyze(String mood){
+        if(mood.equals("HAPPY"))
+            return "HAPPY";
+        else
+            return "SAD";
+    }
+}
---
+++ src/test/java/com/bridgelabz/LambdaExpression/Day23/DetailsTest.java
@@ -0,0 +1,41 @@
+package com.bridgelabz.LambdaExpression.Day23;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DetailsTest {
+
+    @Test
+    public void moodtest() {
+        String expectedresult = "HAPPY";
+        String actualresult = HappyOrSad.moodanalyze("HAPPY");
+        assertEquals(expectedresult, actualresult);
+    }
+
+
+    @Test
+    public void firstname() {
+        String firstname="Ramit";
+        assertTrue(ValidateDetailsTest.name(firstname));
+    }
+    @Test
+    public void lastname() {
+        String lastname="Kumar";
+        assertTrue(ValidateDetailsTest.name(lastname));
+    }
+    @Test
+    public void mail() {
+        String mail="ramitkumar619@gmail.com";
+        assertTrue(ValidateDetailsTest.mail(mail));
+    }@Test
+    public void phonenumber() {
+        String phonenumber="91 9910936991";
+        assertTrue(ValidateDetailsTest.phonenumber(phonenumber));
+    }@Test
+    public void password() {
+        String password="kumar@1234";
+        assertTrue(ValidateDetailsTest.password(password));
+    }
+}
