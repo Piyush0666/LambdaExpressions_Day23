bdab854b91bdc1fdf59f2ce0465d54df34da373c
your_github_username <your_github_email>
2022-03-10 18:26:43 +0530

    Added:first_name

--- src/main/java/com/bridgelabz/LambdaExpression/Day23/Regex.java
+++ src/main/java/com/bridgelabz/LambdaExpression/Day23/Regex.java
@@ -1,10 +1,27 @@
 package com.bridgelabz.LambdaExpression.Day23;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Regex
 {
-    public static void main( String[] args )
-
-    {
-        System.out.println( " Welcome to User Registration Problem" );
+        public static void main( String[] args ) {
+        Regex regex = new Regex();
+        regex.firstNameInput();
     }
-}
+
+        public void firstNameInput() {
+
+        String firstNameInput = "Piyush";
+        String regex = "^[[a-z]{1}A-Z]{1,}$";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(firstNameInput);
+        if (matcher.matches()) {
+            System.out.println("\n VALID  Name ");
+        } else {
+            System.out.println("\n invalid Name");
+        }
+    }
+
+    }
