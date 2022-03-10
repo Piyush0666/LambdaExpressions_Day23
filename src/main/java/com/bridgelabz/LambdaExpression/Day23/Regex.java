412a23290a466a4a4471d2cfd7b166fcf0642b07
your_github_username <your_github_email>
2022-03-10 18:20:31 +0530

    Welcome Message

---
+++ src/main/java/com/bridgelabz/LambdaExpression/Day23/Regex.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.LambdaExpression.Day23;
+
+public class Regex
+{
+    public static void main( String[] args )
+
+    {
+        System.out.println( " Welcome to User Registration Problem" );
+    }
+}
---
+++ src/test/java/com/bridgelabz/LambdaExpression/Day23/RegexTest.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.LambdaExpression.Day23;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegexTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+}
