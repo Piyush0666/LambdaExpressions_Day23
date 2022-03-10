4ad442fbe1dcc7e893171c709b3407a67879b675
your_github_username <your_github_email>
2022-03-10 18:45:41 +0530

    Added:Password_Rule::2

--- src/main/java/com/bridgelabz/LambdaExpression/Day23/Regex.java
+++ src/main/java/com/bridgelabz/LambdaExpression/Day23/Regex.java
@@ -11,6 +11,7 @@ public class Regex {
         regex.emailInput();
         regex.numberInput();
         regex.passwordInput1();
+        regex.passwordInput2();
     }
 
     public void firstNameInput() {
@@ -57,7 +58,7 @@ public class Regex {
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(numberInput);
         if (matcher.matches()) {
-            System.out.println("\n VALID  mobileNumber ");
+            System.out.println("\n VALID  mobileNumber " );
         } else {
             System.out.println("\n invalid  mobileNumber");
         }
@@ -69,9 +70,21 @@ public class Regex {
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(passwordInput1);
         if (matcher.matches()) {
-            System.out.println("\n VALID  rule 1 passed ");
+            System.out.println("\n VALID  rule 1 passed " );
         } else {
             System.out.println("\n invalid rule 1  not passed ");
+        }
+    }
+
+    public void passwordInput2() {
+        String passwordInput2 = "PiyushVerma";
+        String regex = "^[a-zA-Z]{8,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(passwordInput2);
+        if (matcher.matches()) {
+            System.out.println("\n VALID  rule 2 passed " );
+        } else {
+            System.out.println("\n invalid rule 2  not passed ");
         }
     }
 }
